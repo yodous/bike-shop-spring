b0f5cf4e09e995d8ee5f59f9a8d1069d2e1eec95
Fryderyk <fryderyk32j@gmail.com>
2021-10-26 12:11:49 +0200

    Created distinct controllers for product read and  management methods

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,56 +1,31 @@
 package com.example.controller;
 
-import com.example.dto.ProductCreateRequest;
 import com.example.dto.ProductDTO;
-import com.example.model.ProductUpdate;
 import com.example.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
-    @GetMapping
-    public ResponseEntity<List<ProductDTO>> getAll() {
-        return ResponseEntity.ok(productService.getAll());
+    @GetMapping("/listing")
+    public ResponseEntity<List<ProductDTO>> getByString(@RequestParam String string) {
+        return ResponseEntity.ok(productService.getByString(string));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/sale/{id}")
     public ResponseEntity<ProductDTO> getOne(@PathVariable int id) {
-        return new ResponseEntity<>(productService.get(id), HttpStatus.OK);
+        return ResponseEntity.ok(productService.get(id));
     }
 
-    @PostMapping
-    public ResponseEntity<String> create(@RequestBody ProductCreateRequest productCreateRequest) {
-        productService.create(productCreateRequest);
-        return new ResponseEntity<>("Product has been saved", HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<String> update(@PathVariable int id, @RequestBody ProductUpdate product) {
-        productService.update(id, product);
-
-        return new ResponseEntity<>("Product has been updated", HttpStatus.OK);
-    }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity<String> updatePrice(@PathVariable int id, @RequestBody double newPrice) {
-        productService.updatePrice(id, newPrice);
-        return new ResponseEntity<>("Product price has been updated", HttpStatus.NO_CONTENT);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable int id) {
-        productService.delete(id);
-
-        return new ResponseEntity<>("Product has been deleted", HttpStatus.OK);
+    @GetMapping("/sale/{username}")
+    public ResponseEntity<List<ProductDTO>> getByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(productService.getAllByUsername(username));
     }
 
 }
---
+++ src/main/java/com/example/controller/ProductMgmtController.java
@@ -0,0 +1,42 @@
+package com.example.controller;
+
+import com.example.dto.ProductCreateRequest;
+import com.example.model.ProductUpdate;
+import com.example.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/offers")
+@RequiredArgsConstructor
+public class ProductMgmtController {
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody ProductCreateRequest productCreateRequest) {
+        productService.create(productCreateRequest);
+        return new ResponseEntity<>("Product has been saved", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/offers/{id}")
+    public ResponseEntity<String> update(@PathVariable int id, @RequestBody ProductUpdate product) {
+        productService.update(id, product);
+
+        return new ResponseEntity<>("Product has been updated", HttpStatus.OK);
+    }
+
+    @PatchMapping("/offers/{id}")
+    public ResponseEntity<String> updatePrice(@PathVariable int id, @RequestBody double newPrice) {
+        productService.updatePrice(id, newPrice);
+        return new ResponseEntity<>("Product price has been updated", HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/offers/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        productService.delete(id);
+
+        return new ResponseEntity<>("Product has been deleted", HttpStatus.NO_CONTENT);
+    }
+}
