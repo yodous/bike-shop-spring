320f6b33f3240364f225484b33fedc022745c1c5
Fryderyk <fryderyk32j@gmail.com>
2021-11-14 08:03:40 +0100

    test: Added AuthServiceImplTest

--- src/test/java/com/example/service/impl/AuthServiceImplTest.java
+++ src/test/java/com/example/service/impl/AuthServiceImplTest.java
@@ -1,32 +1,39 @@
 package com.example.service.impl;
 
-import com.example.repository.AccountActivationTokenRepository;
+import com.example.dto.RegisterRequest;
+import com.example.exception.InvalidPasswordException;
+import com.example.exception.UsernameTakenException;
+import com.example.mapper.UserViewMapper;
+import com.example.model.User;
 import com.example.repository.UserRepository;
-import com.example.service.EmailService;
-import com.example.validation.SignupValidator;
+import com.example.security.JwtTokenService;
+import com.example.service.MessageSender;
+import com.example.validation.RegisterValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class AuthServiceImplTest {
     @Mock
+    private JwtTokenService jwtTokenService;
+    @Mock
     private UserRepository userRepository;
     @Mock
-    private AccountActivationTokenRepository authTokenRepository;
+    private MessageSender messageSender;
     @Mock
-    private EmailService emailService;
+    private RegisterValidator registerValidator;
     @Mock
-    private PasswordEncoder passwordEncoder;
-    @Mock
-    private SignupValidator signupValidator;
+    private UserViewMapper userViewMapper;
+
     @InjectMocks
-    AuthServiceImpl service;
+    private AuthServiceImpl service;
 
     @BeforeEach
     void setup() {
@@ -34,19 +41,50 @@ class AuthServiceImplTest {
     }
 
     @Test
-    void signup_WhenSignupRequestIsValid_ThenOk() {
-//        Address testAddress = new Address("city", "street", "12-345");
-//        User user = new User("user", "password", "firstname",
-//                "lastname", "test@email.io", "1234 5678 8765 4321", new UserRole(Role.USER), testAddress);
-//        SignupRequest signupRequest = new SignupRequest(
-//                "user", "password", "password", "firstname", "lastname",
-//                "test@email.io", "1234 5678 8765 4321", "city", "street", "12-345", "USER");
-//        when(userRepository.save(any())).thenReturn(user);
-//
-//        service.signup(signupRequest);
-//
-//        verify(signupValidator).validate(signupRequest);
-//        verify(emailService).sendMessage(any());
+    void registerWithValidRegisterRequest_ThenOK() {
+        RegisterRequest request = new RegisterRequest();
+        User user = new User();
+        when(userViewMapper.mapRegisterRequestToUser(request)).thenReturn(user);
+        when(userRepository.save(any())).thenReturn(user);
+        when(jwtTokenService.generateAccessToken(any())).thenReturn("test_access_token");
+
+        service.register(request);
+
+        verify(registerValidator).validate(request);
+        verify(userViewMapper).mapRegisterRequestToUser(request);
+        verify(jwtTokenService).generateAccountActivationToken(user);
+        verify(messageSender).sendMessage(any());
     }
 
+    @Test
+    void registerWithUsernameTaken_ThenThrowUsernameTakenException() {
+        RegisterRequest request = new RegisterRequest();
+        doThrow(UsernameTakenException.class).when(registerValidator).validate(any());
+
+        Exception exception = assertThrows(UsernameTakenException.class,
+                () -> service.register(request));
+
+        assertThat(exception).isInstanceOf(UsernameTakenException.class);
+        verify(registerValidator).validate(request);
+
+        verify(userViewMapper, never()).mapRegisterRequestToUser(request);
+        verify(jwtTokenService, never()).generateAccountActivationToken(any());
+        verify(messageSender, never()).sendMessage(any());
+    }
+
+    @Test
+    void registerWithInvalidPassword_ThenThrowInvalidPasswordException() {
+        RegisterRequest request = new RegisterRequest();
+        doThrow(InvalidPasswordException.class).when(registerValidator).validate(any());
+
+        Exception exception = assertThrows(InvalidPasswordException.class,
+                () -> service.register(request));
+
+        assertThat(exception).isInstanceOf(InvalidPasswordException.class);
+        verify(registerValidator).validate(request);
+
+        verify(userViewMapper, never()).mapRegisterRequestToUser(request);
+        verify(jwtTokenService, never()).generateAccountActivationToken(any());
+        verify(messageSender, never()).sendMessage(any());
+    }
 }
