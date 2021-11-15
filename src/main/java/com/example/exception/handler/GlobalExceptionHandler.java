ab2bd90727d11fb128f6a249eba47f686e5b1c6f
Fryderyk <fryderyk32j@gmail.com>
2021-11-15 12:59:13 +0100

    refactor: moved Auth/ProdcutControllerAdvisor to GlobalExceptionHandler

--- src/main/java/com/example/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.exception.handler;
 
 import com.example.exception.InvalidPasswordException;
 import com.example.exception.InvalidTokenException;
+import com.example.exception.ProductNotFoundException;
 import com.example.exception.UsernameTakenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,26 +11,30 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class AuthControllerAdvisor {
-
+public class GlobalExceptionHandler {
+    
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<Object> loginExceptionHandler(AuthenticationException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Object> handleAuthenticationException(AuthenticationException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(UsernameTakenException.class)
-    public ResponseEntity<Object> usernameTakenHandler(UsernameTakenException exception) {
+    public ResponseEntity<Object> handleUsernameTakenException(UsernameTakenException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<Object> invalidPasswordHandler(InvalidPasswordException exception) {
+    public ResponseEntity<Object> handleInvalidPasswordException(InvalidPasswordException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<Object> tokenExpiredHandler(InvalidTokenException exception) {
+    public ResponseEntity<Object> handleInvalidTokenException(InvalidTokenException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
+    @ExceptionHandler(value = ProductNotFoundException.class)
+    public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
