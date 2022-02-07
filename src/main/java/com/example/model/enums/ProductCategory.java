10284ec81bbe91f04984da3a783e8b0554e4dcbf
JFrred <fryderyk32j@gmail.com>
2022-02-07 15:26:41 +0100

    change road category image

--- src/main/java/com/example/model/enums/ProductCategory.java
+++ src/main/java/com/example/model/enums/ProductCategory.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ProductCategory {
-    ROAD("road", "https://cdn.mos.cms.futurecdn.net/RFYJ7YqbRWcvGHYHrUxNXi.jpg"),
-    GRAVEL("gravel", "https://www.canyon.com/on/demandware.static/-/Sites-canyon-master/default/dwaa39ae23/images/full/Full_2021_/2021/Full_2021_grail-cf-sl-8_2717_bu-bk_P5.png"),
+    ROAD("road", "https://images.squarespace-cdn.com/content/v1/592be1e48419c2e1dd1a7e44/1615531308791-ZH9WJP9LK9RK2S81YJTN/V3Rs-frozen-blue-1.jpg?format=1500w"),
+    GRAVEL("gravel", "https://s14761.pcdn.co/wp-content/uploads/sites/3/2018/06/canyon-inflite-cf-sl-disc-8-team_c1291-1.jpg"),
     MOUNTAIN("mountain", "https://velo.pl/sites/default/files/products/Accent_bikes_MTB_HERO%20CARBON%20Team1.jpg"),
     E_BIKE("e_bike", "https://www.1enduro.pl/wp-content/uploads/2020/04/giant-trance-e-3-pro-2020-800x437.jpg");
 
